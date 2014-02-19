4718fd1e5ba34d7ef01f78feb5bf9308a9b776ad
arvenjadeaguilar <arvenjadeaguilar@ocomsoft.com>
2014-02-20 06:19:46 +0800

    initialize mobile app.

---
+++ eSurvey-mobile/js/homeController.js
@@ -0,0 +1,10 @@
+
+eSurveyApp.controller('homeController',function($scope,$routeParams,$rootScope,$http,$location){
+	
+	
+	$scope.userProfile = {
+		name:"arven jade aguilar", username:"arvenjadeaguilar", password:"yes",gender:"male", employeeId:"x444", contactNo:"09091234567",team:"team gwapo" }
+
+
+});
+
